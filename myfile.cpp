3b56c45ac0a5ecbaa9546155dda7deffc62a2a07
alexander jxnsson <alexander.jxnsson@gmail.com>
2021-11-09 16:37:05 +0100

    kommit en bit, men fastnat på functionen för att kolla alla användare. klart för idag.

--- myfile.cpp
+++ myfile.cpp
@@ -1,40 +1,116 @@
+/* declarations start */
 #include <iostream>
 #include <iomanip>
 #include <string>
 #include <vector>
-using namespace std;
+#include <bits/stdc++.h>
+bool menu = true, accountLoginStopper = true;
+int menuChoice;
+void atmMenu();
+enum MenuChoice{CreateAccount = 1, LogIn};
 struct AutoTellerMachine
 {
-    //n
-};
-// to represent each customer who uses the ATM program
-//Create new accout
-void CreateNewAccount(string newUsername, string newPassword);
-//Log in, if/case to compare.
-void AccountLogin(string loginUsername, string loginPassword);
-//Depost money++
-void DepositMoney(double depositAmount);
-//Withdraw money--
-void WithdrawMoney(double withdrawalAmount);
+    void CreateNewAccount(std::string newUsername, std::string newPassword)
+    {
+        username = newUsername;
+        password = newPassword;
+    }
+    void AccountLogin(std::string loginUsername, std::string loginPassword)
+    {
+        if (loginUsername == username && loginPassword == password)
+        {
+            std::cout<<"You have logged in!"<<std::endl;
+            menu = false;
+            accountLoginStopper = false;
+        }
+        else
+        {
+            std::cout<<"Wrong username or password!"<<std::endl;
+            accountLoginStopper = false;
+        }
+    }
+    void DepositMoney(double depositAmount);
+    void WithdrawMoney(double withdrawalAmount);
+    void SetAccountLogin(int setAccountLocation);
+    void SetLastMoneyMovement(int accountID, double amount);
+    void SetBeginningBalance(int accountID);
+    void SetLastOperation(int accountID, char userInput);
+    void AccountMenu();
+    int GetAccountLogin() const;
+    double GetLastMoneyMovement(int accountID) const;
+    double GetAccountBalance(int accountID) const;
+    double GetBeginningBalance(int accountID) const;
+    char GetLastOperation(int accountID) const;
+    std::string GetUsername(int accountID) const;
 
-void SetAccountLogin(int setAccountLocation);
-void SetLastMoneyMovement(int accountID, double amount);
-void SetBeginningBalance(int accountID);
-void SetLastOperation(int accountID, char userInput);
-void AccountMenu();
-int GetAccountLogin() const;
-double GetLastMoneyMovement(int accountID) const;
-double GetAccountBalance(int accountID) const;
-double GetBeginningBalance(int accountID) const;
-char GetLastOperation(int accountID) const;
-string GetUsername(int accountID) const;
-
-int main(){
     int loggedInAccountLocation;
     double accountBalance;
     double beginningBalance;
     double lastMoneyMovement;
     char lastOperation;
-    string username;
-    string password;
+    std::string username;
+    std::string password;
+};
+AutoTellerMachine users[10];
+
+/* declarations end */
+
+/* init start */
+int main(){
+    std::string username, password;
+    AutoTellerMachine admin;
+    
+    int i = 0;
+    //Login loop
+    while (menu)
+    {
+        atmMenu();
+        std::cin>>menuChoice;
+        bool addingAccount = true;
+        switch (menuChoice)
+        {
+            case CreateAccount:
+                std::cout<<"Creating account: \n";
+                std::cout<<"Enter a username: ";
+                std::cin>>username;
+                std::cout<<"Enter password: ";
+                std::cin>>password;
+                //admin.CreateNewAccount(username, password);
+                while (addingAccount)
+                {
+                    users[i].username = username;
+                    users[i].password = password;
+                    i++;
+                    addingAccount = false;
+                }
+                break;
+            case LogIn:
+                std::cout<<"Login: \n";
+                std::cout<<"Enter a username: ";
+                std::cin>>username;
+                std::cout<<"Enter password: ";
+                std::cin>>password;
+                //admin.AccountLogin(username, password);
+                for (int j = 0; j < 10; j++)
+                {
+                    users[j].AccountLogin(username, password);
+                }
+                
+                std::cout<<"Loading . . ."<<std::endl;
+                break;
+            default:
+                std::cout<<"Enter a legit command."<<std::endl;
+                break;
+        }
+    }
+    
 }
+/* init end */
+
+/* functions start */
+void atmMenu()
+{
+    std::cout<<"1. Create account\n2. Log in"<<std::endl;
+}
+
+/* functions end */
