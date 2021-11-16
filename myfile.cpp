b2de35254e7c177a33a9c0633dcab3f19f56f478
alexander jxnsson <alexander.jxnsson@gmail.com>
2021-11-16 11:06:03 +0100

    småttingar

--- myfile.cpp
+++ myfile.cpp
@@ -140,6 +140,26 @@ void ATM::SetAccountLogin(int setAccountLocation)
     tadmin.loggedInAccountLocation = setAccountLocation;
 }
 
+void ATM::SetLastOperation(int accountID, char userInput)
+{
+    tadmin.tUsersList[accountID].lastOperation = userInput;
+}
+
+double ATM::GetLastMoneyMovement(int accountID) const
+{
+    return tadmin.tUsersList[accountID].lastMoneyMovement;
+}
+
+void ATM::SetBeginningBalance(int accountID)
+{
+    tadmin.tUsersList[accountID].beginningBalance = tadmin.tUsersList[accountID].accountBalance;
+}
+
+void ATM::SetLastMoneyMovement(int accountID, double amount)
+{
+    tadmin.tUsersList[accountID].lastMoneyMovement = amount;
+}
+
 void ATM::AccountMenu()
 {
     enum LoggiedInchoice{eDepositMoney = 1, eWithdrawMoney, eRequestBalance, eLogut};
@@ -216,26 +236,6 @@ int ATM::GetAccountLogin() const
 double ATM::GetAccountBalance(int accountID) const
 {
     return tadmin.tUsersList[accountID].accountBalance;
-}
-
-void ATM::SetLastMoneyMovement(int accountID, double amount)
-{
-    tadmin.tUsersList[accountID].lastMoneyMovement = amount;
-}
-
-void ATM::SetBeginningBalance(int accountID)
-{
-    tadmin.tUsersList[accountID].beginningBalance = tadmin.tUsersList[accountID].accountBalance;
-}
-
-void ATM::SetLastOperation(int accountID, char userInput)
-{
-    tadmin.tUsersList[accountID].lastOperation = userInput;
-}
-
-double ATM::GetLastMoneyMovement(int accountID) const
-{
-    return tadmin.tUsersList[accountID].lastMoneyMovement;
 }
 
 double ATM::GetBeginningBalance(int accountID) const
